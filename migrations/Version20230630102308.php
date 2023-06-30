564d2d390ec3b53bad3c5c4fd2f72bb8e931a197
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-06-30 12:33:03 +0200

    Fix Doctrine migration

--- migrations/Version20230630102308.php
+++ migrations/Version20230630102308.php
@@ -7,7 +7,7 @@ namespace CommerceWeaversSyliusAlsoBoughtMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230613113124 extends AbstractMigration
+final class Version20230630102308 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -17,7 +17,8 @@ final class Version20230613113124 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE sylius_also_bought_product_synchronization (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', start_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, number_of_orders INT NOT NULL, affected_products JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE sylius_product ADD bought_together_products JSON NOT NULL DEFAULT (JSON_OBJECT())');
+        $this->addSql('ALTER TABLE sylius_product ADD bought_together_products JSON NOT NULL');
+        $this->addSql('UPDATE sylius_product SET bought_together_products = JSON_ARRAY()');
     }
 
     public function down(Schema $schema): void
